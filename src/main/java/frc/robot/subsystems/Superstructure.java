8ee46c7d30b46514f07faa5c86b7cf213f6edaa2
pedroglz <pedroglzsica@gmail.com>
2024-03-02 19:56:43 -0600

    Superstructure

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -1,5 +1,24 @@
 package frc.robot.subsystems;
 
 public class Superstructure {
-    
+    private static RobotStatus m_robotStatus = RobotStatus.HOME;
+    public static enum RobotStatus{
+        AIMING,
+        AIMED,
+        PICKING_FROM_SOURCE,
+        PICKING_FROM_FLOOR,
+        SHOOTING,
+        HOME,
+        ELEVATING,
+        SCORING_IN_AMP,
+        ALIGNING_TO_AMP
+    }
+
+    public static RobotStatus getRobotStatus(){
+        return m_robotStatus;
+    }
+
+    public static void setRobotStatus(RobotStatus _RobotStatus){
+        m_robotStatus = _RobotStatus;
+    }
 }
