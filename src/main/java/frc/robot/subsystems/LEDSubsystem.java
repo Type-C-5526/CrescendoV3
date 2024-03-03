aa4ca6c1c3a457095f9592cfaafe19c958c56963
pedroglz <pedroglzsica@gmail.com>
2024-03-02 19:56:33 -0600

    LED Subsystem

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -4,14 +4,67 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.LEDs;
+import frc.robot.subsystems.Superstructure.RobotStatus;
 
 public class LEDSubsystem extends SubsystemBase {
+  private AddressableLED m_leds;
+  private AddressableLEDBuffer m_ledsbuffer;
   /** Creates a new LEDSubsystem. */
-  public LEDSubsystem() {}
+  public LEDSubsystem() {
+
+    m_leds = new AddressableLED(LEDs.LedPort);
+    m_ledsbuffer = new AddressableLEDBuffer(LEDs.LedLength);
+
+    m_leds.setLength(m_ledsbuffer.getLength());
+
+    m_leds.setData(m_ledsbuffer);
+
+    m_leds.start();
+
+
+
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    m_leds.setData(m_ledsbuffer);
+
+    switch (Superstructure.getRobotStatus()){
+      case AIMED:
+        
+        break;
+      case AIMING:
+
+        break;
+    }
+      
   }
+
+  public void solid(){
+
+  }
+  public void Ascend(Color color) {
+  
+    }
+
+  public void blink(){
+
+  }
+  public void epilepticAttack(){
+
+  }
+
+  public void fire(){
+
+  }
+  public void typeCMorseCode(){
+
+  }
+
 }
